e3a7dfe04ecedc76890887661fc4d894797f63ce
yedio <yediobjet@gmail.com>
2022-09-24 11:36:06 +0900

    Initial Setting

---
+++ test.js
@@ -0,0 +1,19 @@
+/*
+유형: 
+문제: 
+날짜: 22.00.00
+
+[매개변수]
+
+[return]
+
+[조건]
+*/
+
+function solution(n) {
+  var answer = [];
+  return answer;
+}
+
+const n = []; //
+console.log('*RESULT*', solution(n));
